f032730bde132f5405edca40b0170d1c31c4750d
Sejal Srivastav <154933534+ssrivastav01@users.noreply.github.com>
2024-10-30 20:46:39 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// JavaScript for Counter Animation
+document.addEventListener("DOMContentLoaded", function () {
+  const counters = document.querySelectorAll('.counter');
+
+  counters.forEach(counter => {
+    const updateCounter = () => {
+      const target = +counter.getAttribute('data-target');
+      const count = +counter.innerText;
+
+      const increment = target / 200;
+
+      if (count < target) {
+        counter.innerText = Math.ceil(count + increment);
+        setTimeout(updateCounter, 20);
+      } else {
+        counter.innerText = target;
+      }
+    };
+
+    updateCounter();
+  });
+});
